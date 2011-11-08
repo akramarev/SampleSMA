e90d953d4d6f2cadc629c81b9d5f936c02d762aa
Andrew Kramarev <kramarew@ruswizards.com>
2011-11-08 22:33:16 +0400

    Bad try to create parallel optimizer. Eat a lot of memory and get wrong results.

--- EMAStrategyOptimizer.cs
+++ EMAStrategyOptimizer.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using StockSharp.BusinessEntities;
+using System.Threading;
+using System.Threading.Tasks;
+using StockSharp.Algo.Candles;
+using StockSharp.Algo.Indicators.Trend;
 using StockSharp.Algo.Storages;
 using StockSharp.Algo.Testing;
-using StockSharp.Algo.Candles;
-using System.Threading;
-using StockSharp.Algo.Indicators.Trend;
-using Ecng.Serialization;
+using StockSharp.BusinessEntities;
+using StockSharp.Algo;
 
 namespace SampleSMA
 {
@@ -28,6 +28,7 @@ namespace SampleSMA
         }
 
         public EmaStrategy BestStrategy { get; protected set; }
+        private List<EmaStrategy> _performedStrategies = new List<EmaStrategy>();
 
         private readonly TimeSpan _timeFrame = TimeSpan.FromMinutes(5);
 
@@ -38,7 +39,8 @@ namespace SampleSMA
         private TradingStorage _storage;
         private Portfolio _portfolio;
 
-
+        public delegate void StateChangedHandler();
+        public event StateChangedHandler StateChanged;
 
         public EMAStrategyOptimizer(Security security, TradingStorage storage, Portfolio portfolio, DateTime startTime, DateTime stopTime)
         {
@@ -52,6 +54,10 @@ namespace SampleSMA
 
         public void Optimize()
         {
+            // clean up
+            BestStrategy = null;
+            _performedStrategies.Clear();
+
             this.OnStateChanged(OptimizationState.Began);
 
             Thread t = new Thread(new ThreadStart(AsyncOptimize));
@@ -60,91 +66,42 @@ namespace SampleSMA
 
         private void AsyncOptimize()
         {
-            EmulationTrader trader = new EmulationTrader(
-                new[] { _security },
-                new[] { _portfolio })
+            var basketTrader = new BasketTrader { SupportTradesUnique = false };
+
+            OptVarItem[] optVars = this.GetOptVarField().ToArray();
+            ManualResetEvent[] doneEvents = new ManualResetEvent[optVars.Length];
+
+            for (int i = 0; i < optVars.Length; i++)
             {
-                MarketTimeChangedInterval = _timeFrame,
-                Storage = _storage,
-                WorkingTime = Exchange.Rts.WorkingTime,
+                doneEvents[i] = new ManualResetEvent(false);
 
-                // параметр влияет на занимаемую память.
-                // в случае достаточно количества памяти на компьютере рекомендуется его увеличить
-                DaysInMemory = 100,
-            };
-
-            trader.DepthGenerators[_security] = new TrendMarketDepthGenerator(_security)
-            {
-                // стакан для инструмента в истории обновляется раз в 60 (1) секунд
-                Interval = TimeSpan.FromSeconds(10),
-            };
-
-            CandleManager candleManager = new CandleManager();
-
-            var builder = new CandleBuilder(new SyncTraderTradeSource(trader));
-            candleManager.Sources.Add(builder);
-
-            candleManager.RegisterTimeFrameCandles(_security, _timeFrame);
+                EmulationTrader trader = GetOptTraderContext(optVars[i].FilterOptPeriod, optVars[i].LongOptPeriods, optVars[i].ShortOptPeriods, doneEvents[i]);
+                basketTrader.InnerTraders.Add(trader);
+            }
 
             // соединяемся с трейдером и запускаем экспорт,
             // чтобы инициализировать переданными инструментами и портфелями необходимые свойства EmulationTrader
-            trader.Connect();
-            trader.StartExport();
-
-            List<EmaStrategy> performedStrategies = new List<EmaStrategy>();
-
-            // sync object
-            EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.ManualReset); ;
-
-            // prepare optimization sets
-            List<int> filterOptPeriods = new List<int>() { 90, 100, 110 };
-            List<int> longOptPeriods = new List<int>() { 12, 13, 14 };
-            List<int> shortOptPeriods = new List<int>() { 9, 10, 11 };
-
-            foreach (int filterOptPeriod in filterOptPeriods)
+            foreach (var trader in basketTrader.InnerTraders.Cast<IEmulationTrader>())
             {
-                foreach (int longOptPeriod in longOptPeriods)
-                {
-                    foreach (int shortOptPeriod in shortOptPeriods)
-                    {
-                        var strategy = new EmaStrategy(candleManager,
-                            new ExponentialMovingAverage { Length = filterOptPeriod },
-                            new ExponentialMovingAverage { Length = longOptPeriod }, new ExponentialMovingAverage { Length = shortOptPeriod },
-                            _timeFrame)
-                        {
-                            Volume = 1,
-                            Portfolio = _portfolio,
-                            Security = _security,
-                            Trader = trader
-                        };
-
-                        performedStrategies.Add(strategy);
-
-                        trader.StateChanged += () =>
-                        {
-                            if (trader.State == EmulationStates.Started)
-                            {
-                                waitHandle.Reset();
-                                strategy.Start();
-                            }
-                            else if (trader.State == EmulationStates.Stopped)
-                            {
-                                waitHandle.Set();
-                            }
-                        };
-
-                        waitHandle.WaitOne();
-                        trader.Start(_startTime, _stopTime);
-                    }
-                }
+                trader.Connect();
+                trader.StartExport();
             }
 
-            this.BestStrategy = performedStrategies.OrderByDescending(s => s.PnLManager.PnL).FirstOrDefault();
+            // запускаем эмуляцию
+            foreach (var trader in basketTrader.InnerTraders.Cast<IEmulationTrader>())
+            {
+                // указываем даты начала и конца тестирования
+                trader.Start(_startTime, _stopTime);
+            }
+
+            WaitHandle.WaitAll(doneEvents);
+
+            // debug
+            //_performedStrategies.ForEach(s => Console.WriteLine(s.PnLManager.PnL));
+
+            this.BestStrategy = _performedStrategies.OrderByDescending(s => s.PnLManager.PnL).FirstOrDefault();
             this.OnStateChanged(OptimizationState.Finished);
         }
-
-        public delegate void StateChangedHandler();
-        public event StateChangedHandler StateChanged;
 
         protected void OnStateChanged(OptimizationState state)
         {
@@ -154,6 +111,107 @@ namespace SampleSMA
             {
                 StateChanged();
             }
+        }
+
+        private EmulationTrader GetOptTraderContext(int filterOptPeriod, int longOptPeriod, int shortOptPeriod, ManualResetEvent doneEvent)
+        {
+            // clone doesn't work for some reason
+            var security = new Security
+            {
+                Id = _security.Id,
+                Code = _security.Code,
+                Name = _security.Name,
+                MinStepSize = _security.MinStepSize,
+                MinStepPrice = _security.MinStepPrice,
+                Exchange = _security.Exchange,
+            };
+
+            // тестовый портфель
+            var portfolio = new Portfolio { BeginAmount = _portfolio.BeginAmount };
+
+            EmulationTrader trader = new EmulationTrader(
+                new[] { security },
+                new[] { portfolio })
+            {
+                MarketTimeChangedInterval = _timeFrame,
+                Storage = _storage,
+                WorkingTime = Exchange.Rts.WorkingTime,
+
+                // параметр влияет на занимаемую память.
+                // в случае достаточно количества памяти на компьютере рекомендуется его увеличить
+                DaysInMemory = 5,
+            };
+
+            //trader.DepthGenerators[security] = new TrendMarketDepthGenerator(security)
+            //{
+            //    // стакан для инструмента в истории обновляется раз в 1 секунду
+            //    Interval = TimeSpan.FromSeconds(1),
+            //};
+
+            CandleManager candleManager = new CandleManager();
+
+            var builder = new CandleBuilder(new SyncTraderTradeSource(trader));
+            candleManager.Sources.Add(builder);
+
+            candleManager.RegisterTimeFrameCandles(security, _timeFrame);
+
+            var strategy = new EmaStrategy(candleManager,
+                new ExponentialMovingAverage { Length = filterOptPeriod },
+                new ExponentialMovingAverage { Length = longOptPeriod }, new ExponentialMovingAverage { Length = shortOptPeriod },
+                _timeFrame)
+            {
+                Volume = 1,
+                Portfolio = portfolio,
+                Security = security,
+                Trader = trader
+            };
+
+            _performedStrategies.Add(strategy);
+
+            trader.StateChanged += () =>
+            {
+                if (trader.State == EmulationStates.Started)
+                {
+                    strategy.Start();
+                }
+                else if (trader.State == EmulationStates.Stopped)
+                {
+                    doneEvent.Set();
+                }
+            };
+            return trader;
+        }
+
+        public struct OptVarItem
+        {
+            public int FilterOptPeriod;
+            public int LongOptPeriods;
+            public int ShortOptPeriods;
+
+            public OptVarItem(int filterOptPeriod, int longOptPeriods, int shortOptPeriods)
+            {
+                FilterOptPeriod = filterOptPeriod;
+                LongOptPeriods = longOptPeriods;
+                ShortOptPeriods = shortOptPeriods;
+            }
+        }
+
+        private List<OptVarItem> GetOptVarField()
+        {
+            List<OptVarItem> result = new List<OptVarItem>();
+
+            for (int a = 90; a <= 110; a += 10)
+            {
+                for (int b = 12; b <= 14; b += 1)
+                {
+                    for (int c = 9; c < 11; c += 1)
+                    {
+                        result.Add(new OptVarItem(a, b, c));
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
